a213639f36bae2445eca61d573566174b8b6e4f5
jqywang <jqywang.95@gmail.com>
2018-02-08 15:34:16 -0800

    Pass tests in promise constructor and callback review

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,27 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, (err, data) => {
+    if(data) {
+      data = data.toString();
+      var i = data.indexOf('\n');
+      data = data.slice(0,i);
+    }
+    callback(err, data);
+  });
+  //fs.readfile //returns string
+  //check string for index of \n
+  //return string.slice(0,index);
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
   // TODO
+  request(url, function (error, response, body) {
+    response === undefined ? callback(error) : callback(error, response.statusCode);
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -11,11 +11,45 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   // TODO
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.toString());
+      }
+    });
+  })
+
+  // promise()
+  .then(function(result){
+    var index = result.indexOf('\n');
+    result = result.slice(0,index);
+    return result;
+  });
+
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
   // TODO
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, response, body){
+      if(error) {
+        reject(error);
+      } else {
+        if(response) {
+          resolve(response);
+        }
+      }
+    });
+  })
+  .then(function (result) {
+    return result.statusCode;
+  });
+  // request(url, function (error, response, body) {
+  //   response === undefined ? callback(error) : callback(error, response.statusCode);
+  // });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
